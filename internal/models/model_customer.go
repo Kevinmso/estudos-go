2dbedcacd1a1067ef29ea0f63ae1011c5059abf5
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-10-22 15:47:48 -0300

    (feat) Corrige tipo de CustomerId em model_customer.go

--- internal/models/model_customer.go
+++ internal/models/model_customer.go
@@ -1,7 +1,7 @@
 package models
 
 type Customer struct {
-	CustomerId int    `gorm:"primary_key" json:"customerId"`
+	CustomerId string `gorm:"primary_key" json:"customerId"`
 	FirstName  string `json:"firstName"`
 	LastName   string `json:"lastName"`
 	Email      string `json:"email"`
