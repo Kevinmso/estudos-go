3a7e876d698a412e9d6909651d509c68b3f60345
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-10-22 14:13:32 -0300

    (feat) Cria teste de conexão simples na main

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/kevinmso/estudos-go/internal/database"
+	"github.com/kevinmso/estudos-go/internal/server"
+)
+
+func main() {
+	db, err := database.NewDatabaseClient()
+	if err != nil {
+		log.Fatal("failed to connect to database, %s", err)
+	}
+	srv := server.NewEchoServer(db)
+	if err := srv.Start(); err != nil {
+		log.Fatal("failed to start server, %s", err)
+	}
+}
